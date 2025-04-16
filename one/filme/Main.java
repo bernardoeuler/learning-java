2d9ec1df46b8c46330f795838dc00c13d57a56e2
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-04-16 02:34:19 -0300

    Dados do filme

---
+++ one/filme/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String args[]) {
+        System.out.println("Este é o Screen Match");
+        System.out.println("Filme: Top Gun: Maverick");
+
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        float notaDoFilme = 8.1f;
+
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Incluído no plano: " + (incluidoNoPlano ? "Sim" : "Não"));
+        System.out.println("Nota do filme: " + notaDoFilme);
+    }
+}
