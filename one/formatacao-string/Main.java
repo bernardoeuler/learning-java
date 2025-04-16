b1802916d8aa61034e7a076a0fb5f15aa8413263
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-04-16 14:56:15 -0300

    Aprendendo a formatar strings com formatted

---
+++ one/formatacao-string/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String args[]) {
+        String nome = "Bernardo";
+        int idade = 19;
+
+        System.out.println("Olá! Eu me chamo %s e tenho %d anos.".formatted(nome, idade));
+    }
+}
