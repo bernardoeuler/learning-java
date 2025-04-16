fdd22cbc1e49a38cd45257784174d08c6e931175
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-04-16 02:22:52 -0300

    Primitive types

---
+++ one/tipos-primitivos/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String args[]) {
+        byte b = 127;
+        char c = 255;
+        short s = 32767;
+        int i = 2147483647;
+        long l = Long.MAX_VALUE;
+        boolean bool = true || false;
+    }
+}
