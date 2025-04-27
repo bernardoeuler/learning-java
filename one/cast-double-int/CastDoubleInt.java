6e267b9d0322d24c8d6fe37e5f5676a0dc3f548f
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-04-27 14:06:09 -0300

    Aprendendo a converter double para int

---
+++ one/cast-double-int/CastDoubleInt.java
@@ -0,0 +1,8 @@
+public class CastDoubleInt {
+    public static void main(String args[]) {
+        double d = 123.4653;
+        int i = (int) d;
+
+        System.out.printf("Original: %f\nConvertido: %d\n", d, i);
+    }
+}
